745ac0e8f67d29f053355274c294eeb9c60a2d50
IustinianDrug <drug.iusti@gmail.com>
2016-12-04 18:56:03 +0100

    Working classes

---
+++ BoardGameNetwork/BoardGameNetwork/ChooseLobbyNameForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BoardGameNetwork
+{
+    partial class ChooseLobbyNameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lobbyNameBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose lobby name:";
+            // 
+            // lobbyNameBox
+            // 
+            this.lobbyNameBox.Location = new System.Drawing.Point(15, 25);
+            this.lobbyNameBox.MaxLength = 12;
+            this.lobbyNameBox.Name = "lobbyNameBox";
+            this.lobbyNameBox.Size = new System.Drawing.Size(174, 20);
+            this.lobbyNameBox.TabIndex = 1;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(114, 51);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // ChooseLobbyNameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 84);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.lobbyNameBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ChooseLobbyNameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ChooseLobbyNameForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lobbyNameBox;
+        private System.Windows.Forms.Button okButton;
+    }
+}
---
+++ BoardGameNetwork/BoardGameNetwork/ChooseLobbyNameForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace BoardGameNetwork
+{
+    public partial class ChooseLobbyNameForm : Form
+    {
+        public string lobbyName;
+
+        public ChooseLobbyNameForm()
+        {
+            InitializeComponent();
+            this.DialogResult = DialogResult.Abort;
+            this.lobbyNameBox.Text = "Lobby" + new Random().Next(10, 99);
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            lobbyName = lobbyNameBox.Text;
+            if (lobbyName.Length < 3) (new Thread(() => Application.Run(new ErrorForm("Error", "Lobby name too short")))).Start();
+            else
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+    }
+}
