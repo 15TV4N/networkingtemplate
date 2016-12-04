1afff548a7923a605ee613aed805ca0308744b38
15TV4N <15tv4n.k0v4c5@gmail.com>
2016-12-04 21:06:11 +0100

    Project files update

--- BoardGameNetwork/BoardGameNetwork/Properties/AssemblyInfo.cs
+++ BoardGameNetwork/BoardGameNetwork/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("84977c06-6de9-47c3-9092-383a8aeb889f")]
+[assembly: Guid("35d56bb7-68d8-4b7d-8ba2-cdf5c2ac27e4")]
 
 // Version information for an assembly consists of the following four values:
 //
