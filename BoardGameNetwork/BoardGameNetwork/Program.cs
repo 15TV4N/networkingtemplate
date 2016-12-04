9ce005d97bcd45d8386be186bd9fb3d606c9cba0
15TV4N <15tv4n.k0v4c5@gmail.com>
2016-12-04 14:53:51 +0100

    Main entry change
    Will start a server and 3 clients

--- BoardGameNetwork/BoardGameNetwork/Program.cs
+++ BoardGameNetwork/BoardGameNetwork/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
 
 namespace BoardGameNetwork
 {
+    /// <summary>
+    /// The program will run a 'console app' for the server and
+    /// a 'chain' of forms for all the clients described below.
+    /// TCP protocol is used to communicate through local ports.
+    /// </summary>
     static class Program
     {
         /// <summary>
@@ -16,6 +20,21 @@ namespace BoardGameNetwork
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            //one server ONLY
+            var serverMainThread = new Thread(() => new BGServerApp());
+            serverMainThread.Priority = ThreadPriority.Highest;
+            serverMainThread.Start();
+
+            /* 
+             * The points refer to points on screen
+             * It is advised to have at least a screen resolution of 1024x768
+             * (some forms' start location attributes are set to manual input
+             * and will spawn based on these values)
+             */
+            (new Thread(() => new BGClientApp(new Point(500, 100)))).Start();
+            (new Thread(() => new BGClientApp(new Point(200, 400)))).Start();
+            (new Thread(() => new BGClientApp(new Point(500, 400)))).Start();
         }
     }
 }
